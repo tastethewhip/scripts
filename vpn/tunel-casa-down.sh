3d45dc4888faa3e47c647b38605e8615e587a9af
avances123 <avances123@gmail.com>
2009-10-19 21:45:34 +0200

    organizando vpn

--- vpn/tunel-casa-down.sh
+++ vpn/tunel-casa-down.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cp -pf resolv.orig /etc/resolv.conf
+cp -pf /etc/resolv.orig /etc/resolv.conf
 #cp -pf resolv-tinel.conf /etc/resolv.conf
 /etc/init.d/openvpn stop
 
--- vpn/tunel-casa-up.sh
+++ vpn/tunel-casa-up.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cp -pf /etc/resolv.conf resolv.orig
+cp -pf /etc/resolv.conf /etc/resolv.orig
 cp -pf resolv-tunel.conf /etc/resolv.conf
 /etc/init.d/openvpn restart
 
