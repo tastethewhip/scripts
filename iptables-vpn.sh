9cf7c0b9dec1c5ceb7cbf7cf4995020efeca69d1
Fabio Rueda Carrascosa <fabio@fabio.intranet.meteologica.com>
2009-10-19 13:04:53 +0200

    la

---
+++ iptables-vpn.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+/etc/init.d/openvpn restart
+iptables -A FORWARD -i tun+ -j ACCEPT
+ip ro sh
+echo 1 > /proc/sys/net/ipv4/ip_forward
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+#iptables -t nat -A POSTROUTING -o br0 -j MASQUERADE
