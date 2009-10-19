a1a417a94356077ff232c5fc2d9eee4c0425497d
avances123 <avances123@gmail.com>
2009-10-19 22:05:28 +0200

    :

--- vpn/tunel-casa-up.sh
+++ vpn/tunel-casa-up.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 cp -pf /etc/resolv.conf /etc/resolv.orig
-cp -pf resolv-tunel.conf /etc/resolv.conf
+echo "domain intranet.meteologica.com" > /etc/resolv.conf
+echo "domain prod.meteologica.com" >> /etc/resolv.conf
+echo "nameserver 172.20.1.50" >> /etc/resolv.conf
 /etc/init.d/openvpn restart
 
