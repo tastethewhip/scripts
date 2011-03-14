88df74b40dfc12ad976809dc276696e2fb1950df
avances123 <avances123@gmail.com>
2011-03-14 22:20:20 +0100

    tunel casa modified

--- vpn/tunel-casa-up.sh
+++ vpn/tunel-casa-up.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 
-cp -pf /etc/resolv.conf /etc/resolv.orig
-echo "domain intranet.meteologica.com" > /etc/resolv.conf
-echo "domain prod.meteologica.com" >> /etc/resolv.conf
-echo "nameserver router" >> /etc/resolv.conf
-echo "nameserver 172.20.1.50" >> /etc/resolv.conf
-/etc/init.d/openvpn restart
+/etc/init.d/openvpn restart && cat $(dirname $0)/dns-oficina | resolvconf -a eth1
 
