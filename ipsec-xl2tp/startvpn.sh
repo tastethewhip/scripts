4cd48adb0f44e80557accd5b46b595289df5ff59
avances123 <avances123@gmail.com>
2011-05-28 10:11:17 +0200

    scripts funcionando bien

--- ipsec-xl2tp/startvpn.sh
+++ ipsec-xl2tp/startvpn.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
+echo /etc/init.d/ipsec start
 /etc/init.d/ipsec start
-sleep 10                                                   #delay to ensure that IPsec is started before overlaying L2TP
+sleep 5                                                   #delay to ensure that IPsec is started before overlaying L2TP
+echo /etc/init.d/xl2tpd start
 /etc/init.d/xl2tpd start
-sleep 10
+sleep 5
+echo "/bin/echo \"c username\" > /var/run/xl2tpd/l2tp-control"
 /bin/echo "c username" > /var/run/xl2tpd/l2tp-control     
 #PPP_GW_ADD=`./getip.sh ppp0`
 
-ip ro add 10.10.223.0/24 via 10.64.64.64 dev ppp0
-ip ro add 10.10.225.0/24 via 10.64.64.64 dev ppp0
-ip ro add 172.20.0.0/21 via 10.64.64.64 dev ppp0
-ip ro del default via 10.64.64.64 dev ppp0 
-
-
-#route add 68.68.32.79 gw 192.168.1.1 eth0
-#route add default gw $PPP_GW_ADD
-#route delete default gw 192.168.1.1
--- ipsec-xl2tp/stopvpn.sh
+++ ipsec-xl2tp/stopvpn.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+echo "/bin/echo \"d username\" > /var/run/xl2tpd/l2tp-control"
 /bin/echo "d username" > /var/run/xl2tpd/l2tp-control
+sleep 2
+echo /etc/init.d/xl2tpd stop
 /etc/init.d/xl2tpd stop
+sleep 5
+echo /etc/init.d/ipsec stop
 /etc/init.d/ipsec stop
 
 #route delete 68.68.32.79 gw 192.168.1.1 eth0
