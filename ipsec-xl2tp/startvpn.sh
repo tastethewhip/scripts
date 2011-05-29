65af079c04b54a1ea5975822bb4a2b53704fe759
avances123 <avances123@gmail.com>
2011-05-29 22:56:22 +0200

    despues de hacerlo funcionar en cuenca

--- ipsec-xl2tp/startvpn.sh
+++ ipsec-xl2tp/startvpn.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+IFNAME="ppp0"
+ifconfig |grep ppp0 >/dev/null 2>/dev/null
+if [ $? -eq 0 ];then  # La interfaz ppp0 ya esta utilizada (lo mas probable es que por uno de nuestros pinchos)
+	IFNAME="ppp1" # Si teniamos dos conexiones ppp la del tunel seria la ppp2, si eso pasa, nos jodemos)
+fi
 echo /etc/init.d/ipsec start
 /etc/init.d/ipsec start
 sleep 5                                                   #delay to ensure that IPsec is started before overlaying L2TP
@@ -8,5 +13,22 @@ echo /etc/init.d/xl2tpd start
 sleep 5
 echo "/bin/echo \"c username\" > /var/run/xl2tpd/l2tp-control"
 /bin/echo "c username" > /var/run/xl2tpd/l2tp-control     
-#PPP_GW_ADD=`./getip.sh ppp0`
+
+sleep 10
+
+IPREMOTE=`./getip.sh $IFNAME`
+
+
+echo ip ro add 10.10.223.0/24 via $IPREMOTE dev $IFNAME
+echo ip ro add 10.10.225.0/24 via $IPREMOTE dev $IFNAME
+echo ip ro add 172.20.0.0/21 via $IPREMOTE dev $IFNAME
+
+ip ro add 10.10.223.0/24 via $IPREMOTE dev $IFNAME
+ip ro add 10.10.225.0/24 via $IPREMOTE dev $IFNAME
+ip ro add 172.20.0.0/21 via $IPREMOTE dev $IFNAME
+
+
+sleep 2
+
+ping -c3 gregal.intranet.meteologica.com
 
--- ipsec-xl2tp/stopvpn.sh
+++ ipsec-xl2tp/stopvpn.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+IFNAME="ppp0"
+ifconfig |grep ppp0 >/dev/null 2>/dev/null
+if [ $? -eq 0 ];then  # La interfaz ppp0 ya esta utilizada (lo mas probable es que por uno de nuestros pinchos)
+	IFNAME="ppp1" # Si teniamos dos conexiones ppp la del tunel seria la ppp2, si eso pasa, nos jodemos)
+fi
+
+
+IPREMOTE=`./getip.sh $IFNAME`
+
 echo "/bin/echo \"d username\" > /var/run/xl2tpd/l2tp-control"
 /bin/echo "d username" > /var/run/xl2tpd/l2tp-control
-sleep 2
+sleep 5
 echo /etc/init.d/xl2tpd stop
 /etc/init.d/xl2tpd stop
-sleep 5
+sleep 2
 echo /etc/init.d/ipsec stop
 /etc/init.d/ipsec stop
 
-#route delete 68.68.32.79 gw 192.168.1.1 eth0
-#route add default gw 192.168.1.1
